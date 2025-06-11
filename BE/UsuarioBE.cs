89aaa6412acefd050de47f80391089b99be8928a
gilazo1996 <gaston_roa@live.com>
2025-06-11 17:52:47 -0300

    agregando carpeta de SRS, incluyendo diagramas del proyecto

---
+++ BE/UsuarioBE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public abstract class UsuarioBE
+    {
+        public string _email;
+        public string _password;
+        public string _nickname;
+        public string _nombre;
+        public string _apellido;
+        public char _dni;
+        public string _genero;
+        public string _nroCelular;
+        public DateTime _fechaNacimiento;
+        public int _idEstado;
+    }
+}
