cbc4ef2e439b24fd9fbde4422cb3215c4b7c9bbb
Lautaropp <pelozo.lautaro.e@gmail.com>
2025-07-03 18:31:47 -0300

    Login modificado, funcionalidades para Gerente y Administrador agregadas

--- UI/Gerente/FrmGerente.Designer.cs
+++ UI/Gerente/FrmGerente.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FarmaSalud.Gerente
+﻿namespace FarmaSalud
 {
     partial class FrmGerente
     {
--- UI/Gerente/FrmGerente.cs
+++ UI/Gerente/FrmGerente.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FarmaSalud.Gerente
+namespace FarmaSalud
 {
     public partial class FrmGerente : Form
     {
